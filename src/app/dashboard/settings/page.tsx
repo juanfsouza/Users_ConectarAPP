7ffe70a89ba76077600f4cf98cf62c03bd41a865
juanfsouza <juanfsouza@gmail.com>
2025-06-22 14:43:27 -0300

    fix: middleware dependency injection with TypeOrm

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,8 +1,35 @@
+"use client";
+
+import { TypewriterEffectSmooth } from "../../components/ui/typewriter-effect";
+
 export default function SettingsPage() {
+  const words = [
+    {
+      text: "Build",
+    },
+    {
+      text: "challenge",
+    },
+    {
+      text: "apps",
+    },
+    {
+      text: "with",
+    },
+    {
+      text: "User Manager",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
   return (
-    <div>
-      <h1 className="text-xl font-semibold mb-4">Configurações</h1>
-      <p>Em breve...</p>
+    <div className="flex flex-col items-center justify-center h-[40rem]  ">
+      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
+        Creative Makes Agents
+      </p>
+      <TypewriterEffectSmooth words={words} />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+      </div>
     </div>
   );
 }
+
